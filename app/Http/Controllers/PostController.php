b34a26a6b0a95e0155d2e6f2bf61a2964bea73b0
Claudio996 <claudioantonio.retamal@gmail.com>
2023-02-13 15:44:56 -0300

    CRETE ROUTE POSTCONTROLLER AND MIDDLEWARE AUTH

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,41 +7,23 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-  
+
     public function index()
     {
         //
     }
 
-    public function create()
-    {
-        //
-    }
-
-   
     public function store(Request $request)
     {
         //
     }
 
-    public function show(Post $post)
-    {
-        //
-    }
-
-   
-    public function edit(Post $post)
-    {
-        //
-    }
-
-    
     public function update(Request $request, Post $post)
     {
         //
     }
 
-    
+
     public function destroy(Post $post)
     {
         //
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,11 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -24,6 +15,11 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+
+Route::resource('posts',PostController::class)
+->only(['index', 'store', 'update' ,'destroy'])
+->middleware(['auth']);
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
